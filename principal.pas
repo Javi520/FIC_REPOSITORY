26069c4bc8078c23f8de56073f8468879a4b2dc1
Javi520 <javi520@live.com>
2015-04-30 12:01:22 +0200

    cancelacion.pas
    este es solo el procedure cancelacion del programa principla

--- principal.pas
+++ principal.pas
@@ -1,1 +1,23 @@
 //hola estoy probando el sistema de github para hostear un proyecto conjunto
+procedure Cancelacion(cliente:tNombreEspectador;cantidad:integer);
+var i:integer;
+butaca:tPosR;
+Usuario:tPosU;
+begin
+  Usuario:=BuscarDatoU(cliente,listaU);
+  if Usuario=NULOU then writeln("+++ERROR DE CANCELACION: RESERVA INEXISTENTE")
+  else begin
+    butaca:=PrimeraR(listaR);
+    while (i<>cantidad)or(Butaca=NULOU) do begin
+    writeln("Cancelacion de ",cliente);
+          eliminarPosicionR(butaca,listaR);
+          writeln("Butaca",i:0,cliente);
+          butaca:=siguienteR(butaca,listaR);
+          i:=i+1;
+    end;
+    if butaca=NULOU then begin
+    writeln("Eliminando Usuario");
+    eliminarPosicionU(Usuario,listaU);
+    end;
+  end;
+end;
